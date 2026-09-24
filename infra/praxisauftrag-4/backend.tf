0f0906e67266fe0c4b8ccc3faaf67e9838a4edd4
armindoerzbachtbz <89258133+armindoerzbachtbz@users.noreply.github.com>
2026-09-24 16:07:55 +0200

    backend

--- infra/praxisauftrag-4/backend.tf
+++ infra/praxisauftrag-4/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket"  #replace with your bucket name
+    bucket         = "terraform-state-bucket-679328225347-us-east-1-an"  #replace with your bucket name
     key            = "auftrag4.terraform.tfstate" 
     region         = "us-east-1"
   }
